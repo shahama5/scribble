f01c9f1f0764cc86d67c07a239a791ac2a4840b5
shahama5 <shahamaparveen5@gmail.com>
2025-12-04 11:03:46 +0530

    Created a Next.js project
    created A Next.js project and installed dependencies

---
+++ app/_layout/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    <div>
+        <h1 className=''>scribble</h1>
+    </div>
+    </div>
+  )
+}
+
+export default Header
